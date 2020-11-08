3a8084b9582a8c54a010a7154b52a0666550756f
Karla <kcherrera@uh.edu>
2020-11-08 09:41:20 -0600

    Landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,28 +8,31 @@ function Layout({ children }) {
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <Header />
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+      {/* <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+        {children}
+      </main> */}
+      <main className=" flex-1 w-full ">
         {children}
       </main>
 
-      <footer className="bg-blue-700">
+      <footer className="bg-custom-100">
         <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">
-            Created by{` `}
+            Made for{` `}
             <a
               className="font-bold no-underline"
-              href="https://bryant.io"
+              href="https://boston.techtogether.io"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Taylor Bryant
+              TechTogether Boston
             </a>
           </p>
 
           <p>
             <a
               className="font-bold text-white no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              href="https://github.com/kcherrera/monji"
               target="_blank"
               rel="noopener noreferrer"
             >
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ module.exports = {
       'poppins': ['poppins', 'sans-serif'],
       'WorkSans': ['"Work Sans"', 'sans-serif']
     },
-    extend: {},
+
+    extend: {
+      colors: {
+        custom: {
+          100: '#124653',
+          200: '#FE8D8F',
+          300: '#142546',
+          400: '#51BFBD',
+          500: '#019EBC',
+          600: '#009996',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
