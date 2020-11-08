14c62fbb97b64f4a22b3054208f6c2b28f04b5b8
Janna Pierre <gojanna@aol.com>
2020-11-07 21:22:53 -0500

    Added background color to title and sample articleBlocks with links

--- src/components/articleBlock.js
+++ src/components/articleBlock.js
@@ -2,11 +2,21 @@ import React from "react"
 
 export default function ArticleBlock() {
   return (
-    <div style = {{backgroundColor: '#124653',
-    color: 'white'
+    <div style = {{backgroundColor: '#FE8D8F',
+    color: 'white',
+    marginTop:'10px',
     }}>
-      <h3> Article Title </h3>
-      <p>Body text </p>
+      <div class="article">
+        <h3>Women and the Financial Literacy Gap</h3>
+        <p><a href="https://www.opploans.com/oppu/articles/gender-gap-financial-literacy/" target="_blank">Read More </a></p>
+      </div>
+
+      <span>&nbsp;</span>
+
+      <div class="article">
+        <h3>Women: Invest In Your Financial Literacy</h3>
+        <p><a href="https://www.investopedia.com/articles/investing/050813/women-invest-your-financial-literacy.asp" target="_blank">Read More </a></p>
+      </div>
     </div>
   )
 }
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -3,7 +3,9 @@ import ArticleBlock from "../components/articleBlock.js"
 
 export default function Learn() {
   return (
-    <div id="title">
+    <div style = {{backgroundColor: '#124653',
+    color: 'white'
+    }}>
       <h1>Learn Page</h1>
       <ArticleBlock/>
     </div>
