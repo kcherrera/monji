38d9db6f8d250bee43bbd9cdbb6914590f5d7013
Janna Pierre <gojanna@aol.com>
2020-11-07 20:26:30 -0500

    Created Learn page and articleBlock

---
+++ src/components/articleBlock.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function ArticleBlock() {
+  return (
+    <div style = {{backgroundColor: '#124653',
+    color: 'white'
+    }}>
+      <h3> Article Title </h3>
+      <p>Body text </p>
+    </div>
+  )
+}
---
+++ src/pages/learn.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ArticleBlock from "../components/articleBlock.js"
+
+export default function Learn() {
+  return (
+    <div id="title">
+      <h1>Learn Page</h1>
+      <ArticleBlock/>
+    </div>
+
+
+  )
+}
